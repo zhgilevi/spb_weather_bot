21278f94a8bfaff00ea17af6e8ffa7d4a3b8efeb
zhgilevi <38855124+zhgilevi@users.noreply.github.com>
2020-09-05 13:19:32 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import telebot
 import pandas
 
 def get_weather():
-    table=pandas.read_html("https://yandex.ru/pogoda/khanty-mansiysk/details#12" , header=0,parse_dates=['Unnamed: 0'])
+    table=pandas.read_html("https://yandex.ru/pogoda/details?lat=59.875333&lon=29.82581&via=ms" , header=0,parse_dates=['Unnamed: 0'])
 
     answer=[table[0].values[0][0],table[0].values[1][0],table[0].values[2][0],table[0].values[3][0]]
     return answer
 
-token='781229574:AAGC6K39EQ1VNcf2RTOlLpXg_KWoHPAZTIk'
+token='1169550938:AAGMYVG5a7OvafBu8Sq9qXdZpfmyHhVBLqE'
 bot=telebot.TeleBot(token)
 
 @bot.message_handler(commands=['get'])
